717e03f962a980d9d874d654a7289246bda418db
Jeffrey Daniel <jd01680@surrey.ac.uk>
2025-03-31 17:45:41 +0100

    Add initial implementation of Model class and shaders; create level layout and VSCode settings

--- assignment/main.cpp
+++ assignment/main.cpp
@@ -24,9 +24,13 @@ void mouse(int button, int state, int x, int y);
 void motion(int x, int y);
 void Timer(int value);
 void initTexture(std::string filename, GLuint & textureID);
+void drawMaze();
+void drawTank();
+void updateCamera(); // New function to update the camera focus
 int printOglError(char *file, int line);
 
 // Global variables.
+float game_time = 0.0f; // In game timer.
 
 // Screen size.
 int screenWidth   	        = 720;
@@ -35,7 +39,16 @@ int screenHeight   	        = 720;
 //! Array of key states
 bool keyStates[256];
 
+// Tank position tracking
+Vector3f tankPosition = Vector3f(0.0f, 0.0f, 0.0f);
+float tankSpeed = 0.1f;
+float rotationSpeed = 3.0f;
+float tankRotation = 0.0f;
+float turretRotation = 0.0f;
+float wheelRotation = 0.0f;
+
 GLuint shaderProgramID;
+GLuint tankShaderProgramID;
 
 // Viewing/Camera.
 Matrix4x4 ModelViewMatrix;		// ModelView Matrix
@@ -46,19 +59,20 @@ SphericalCameraManipulator cameraManip;
 
 // Meshes.
 Mesh tank_chassis;
+Mesh tank_front_wheel;
+Mesh tank_back_wheel;
+Mesh tank_turret;
+
 Mesh box;
+Mesh coin;
 
 // Hamvee texture.
-GLuint hamvee_vertex_texture_coordinate_attribute; // Vertex Texcoord Attribute Location
-GLuint hamvee_texture_map_uniform_location; // Texture Map Location
+GLuint textureCoordinateAttribute; // Vertex Texcoord Attribute Location
+GLuint textureMapUniformLocation; // Texture Map Location
+GLuint vertexPositionAttribute;		// Vertex Position Attribute Location
 GLuint hamvee_texture; // OpenGL Texture
-GLuint hamvee_vertex_position_attribute;		// Vertex Position Attribute Location
-
-// Box texture.
-GLuint box_vertex_texture_coordinate_attribute;
-GLuint box_texture_map_uniform_location;
-GLuint box_texture;
-GLuint box_vertex_position_attribute;
+GLuint box_texture; // OpenGL Texture
+GLuint coin_texture; // OpenGL Texture
 
 GLuint vertexNormalAttribute;	
 
@@ -73,6 +87,19 @@ Vector3f ambient    = Vector3f(0.1,0.1,0.1);
 Vector3f specular   = Vector3f(0.0,1.0,0.0);
 float specularPower = 10.0;
 
+// Maze
+int maze[10][10] = {
+	{2,1,1,1,1,1,1,1,1,2},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,0,1,1,1,1,1,1,0,1},
+	{1,0,1,0,0,0,0,1,0,1},
+	{1,0,1,0,1,1,0,1,0,1},
+	{1,0,1,0,0,1,0,1,0,1},
+	{1,0,1,1,0,1,0,1,0,1},
+	{1,0,0,0,0,1,0,0,0,1},
+	{1,1,1,1,2,2,1,1,1,1},
+};
+
 //! Main Program Entry
 int main(int argc, char** argv)
 {	
@@ -82,20 +109,28 @@ int main(int argc, char** argv)
 
 	initShader();
 
-	//Init Key States to false;    
+	//Init Key States to false;
     for(int i = 0 ; i < 256; i++)
         keyStates[i] = false;
     
     // Setting up my programme.
 	tank_chassis.loadOBJ("../models/chassis.obj");
+	tank_front_wheel.loadOBJ("../models/front_wheel.obj");
+	tank_back_wheel.loadOBJ("../models/back_wheel.obj");
+	tank_turret.loadOBJ("../models/turret.obj");
 	initTexture("../models/hamvee.bmp", hamvee_texture);
 
 	box.loadOBJ("../models/cube.obj");
 	initTexture("../models/Crate.bmp", box_texture);
 
+	coin.loadOBJ("../models/coin.obj");
+	initTexture("../models/coin.bmp", coin_texture);
+
 	//Init Camera Manipultor
 	cameraManip.setPanTiltRadius(0.f,0.f,2.f);
 	cameraManip.setFocus(tank_chassis.getMeshCentroid());
+
+	glClearColor(0.0,0.33,0.67,1.0);
 
 	//Enter main loop
     glutMainLoop();
@@ -122,7 +157,7 @@ bool initGL(int argc, char** argv)
     glutInitWindowPosition(200, 200);
 
 	//Create Window
-    glutCreateWindow("Tank Assignment");
+    glutCreateWindow("Jeffrey's Tank Assignment");
     
     // Init GLEW
 	if (glewInit() != GLEW_OK) 
@@ -157,15 +192,13 @@ void initShader()
 {
 	//Create shader
     shaderProgramID = Shader::LoadFromFile("shader.vert","shader.frag");
+
     
     // Get a handle for our vertex position buffer
-	hamvee_vertex_position_attribute = glGetAttribLocation(shaderProgramID, "aVertexPosition");
-	box_vertex_position_attribute = glGetAttribLocation(shaderProgramID, "aVertexPosition");
+	vertexPositionAttribute = glGetAttribLocation(shaderProgramID, "aVertexPosition");
 	vertexNormalAttribute = glGetAttribLocation(shaderProgramID,    "aVertexNormal");
-	hamvee_vertex_texture_coordinate_attribute = glGetAttribLocation(shaderProgramID, "aVertexTexcoord");
-	hamvee_texture_map_uniform_location = glGetUniformLocation(shaderProgramID, "TextureMap_uniform");
-	box_vertex_texture_coordinate_attribute = glGetAttribLocation(shaderProgramID, "aVertexTexcoord");
-	box_texture_map_uniform_location = glGetUniformLocation(shaderProgramID, "TextureMap_uniform");
+	textureCoordinateAttribute = glGetAttribLocation(shaderProgramID, "aVertexTexcoord");
+	textureMapUniformLocation = glGetUniformLocation(shaderProgramID, "TextureMap_uniform");
 	
 	// Get ModelView Matrix uniform location
 	MVMatrixUniformLocation = glGetUniformLocation(shaderProgramID, "MVMatrix_uniform"); 
@@ -174,8 +207,6 @@ void initShader()
 	AmbientUniformLocation          = glGetUniformLocation(shaderProgramID, "Ambient_uniform"); 
 	SpecularUniformLocation         = glGetUniformLocation(shaderProgramID, "Specular_uniform"); 
 	SpecularPowerUniformLocation    = glGetUniformLocation(shaderProgramID, "SpecularPower_uniform");
-    hamvee_texture_map_uniform_location       = glGetUniformLocation(shaderProgramID, "TextureMap_uniform");
-	box_texture_map_uniform_location       = glGetUniformLocation(shaderProgramID, "TextureMap_uniform"); 
 }
 
 void initTexture(std::string filename, GLuint & textureID)
@@ -201,8 +232,13 @@ void initTexture(std::string filename, GLuint & textureID)
 //! Display Loop
 void display(void)
 {
+	game_time+= 1.0f; // Increment the in-game timer
+
     //Handle keys
     handleKeys();
+
+    // Update camera to follow tank
+    updateCamera();
 
 	//Set Viewport
 	glViewport(0,0,screenWidth, screenHeight);
@@ -227,41 +263,9 @@ void display(void)
 		false,						//Transpose Matrix
 		ProjectionMatrix.getPtr());	//Pointer to ModelViewMatrixValues
 
-	//Apply Camera Manipluator to Set Model View Matrix on GPU
-    ModelViewMatrix.toIdentity();
+	drawMaze();
+	drawTank();
 
-	//Apply Camera Manipluator to Set Model View Matrix on GPU
-	Matrix4x4 m = cameraManip.apply(ModelViewMatrix);
-	glUniformMatrix4fv(
-		MVMatrixUniformLocation, //Uniform location
-		1, //Number of Uniforms
-		false, //Transpose Matrix
-		m.getPtr()); //Pointer to Matrix Values
-   
-	glUniform3f(LightPositionUniformLocation, lightPosition.x,lightPosition.y,lightPosition.z);
-    glUniform4f(AmbientUniformLocation, ambient.x, ambient.y, ambient.z, 1.0);
-    glUniform4f(SpecularUniformLocation, specular.x, specular.y, specular.z, 1.0);
-    glUniform1f(SpecularPowerUniformLocation, specularPower);
-    
-	// Bind hamvee texture before drawing the tank chassis
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, hamvee_texture);
-	glUniform1i(hamvee_texture_map_uniform_location, 0);
-
-	tank_chassis.Draw(
-		hamvee_vertex_position_attribute,
-		vertexNormalAttribute, hamvee_vertex_texture_coordinate_attribute
-	);
-
-	// Bind box texture before drawing the box
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, box_texture);
-	glUniform1i(box_texture_map_uniform_location, 0);
-
-	box.Draw(
-		box_vertex_position_attribute,
-		vertexNormalAttribute, box_vertex_texture_coordinate_attribute
-	);
 
 	glUseProgram(0);
 
@@ -270,6 +274,144 @@ void display(void)
 	glutPostRedisplay();
 }
 
+void drawMaze()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (maze[i][j] >= 1)
+			{
+				//Apply Camera Manipluator to Set Model View Matrix on GPU
+				ModelViewMatrix.toIdentity();
+
+				//Apply Camera Manipluator to Set Model View Matrix on GPU
+				Matrix4x4 m = cameraManip.apply(ModelViewMatrix);
+				m.translate(i*2, 0, j*-2);
+				glUniformMatrix4fv(
+					MVMatrixUniformLocation, //Uniform location
+					1, //Number of Uniforms
+					false, //Transpose Matrix
+					m.getPtr()); //Pointer to Matrix Values
+
+				glUniform3f(LightPositionUniformLocation, lightPosition.x,lightPosition.y,lightPosition.z);
+				glUniform4f(AmbientUniformLocation, ambient.x, ambient.y, ambient.z, 1.0);
+				glUniform4f(SpecularUniformLocation, specular.x, specular.y, specular.z, 1.0);
+				glUniform1f(SpecularPowerUniformLocation, specularPower);
+				
+				// Bind hamvee texture before drawing the tank chassis
+				glActiveTexture(GL_TEXTURE0);
+				glBindTexture(GL_TEXTURE_2D, box_texture);
+				glUniform1i(textureMapUniformLocation, 0);
+			
+				box.Draw(
+					vertexPositionAttribute,
+					vertexNormalAttribute, textureCoordinateAttribute
+				);
+			}
+			if (maze[i][j] >= 2)
+			{
+				//Apply Camera Manipluator to Set Model View Matrix on GPU
+				ModelViewMatrix.toIdentity();
+
+				//Apply Camera Manipluator to Set Model View Matrix on GPU
+				Matrix4x4 m = cameraManip.apply(ModelViewMatrix);
+				m.translate(i*2, 2, j*-2);
+				m.rotate(game_time, 0, 1, 0); 
+				glUniformMatrix4fv(
+					MVMatrixUniformLocation, //Uniform location
+					1, //Number of Uniforms
+					false, //Transpose Matrix
+					m.getPtr()); //Pointer to Matrix Values
+
+				glUniform3f(LightPositionUniformLocation, lightPosition.x,lightPosition.y,lightPosition.z);
+				glUniform4f(AmbientUniformLocation, ambient.x, ambient.y, ambient.z, 1.0);
+				glUniform4f(SpecularUniformLocation, specular.x, specular.y, specular.z, 1.0);
+				glUniform1f(SpecularPowerUniformLocation, specularPower);
+				
+				// Bind hamvee texture before drawing the tank chassis
+				glActiveTexture(GL_TEXTURE0);
+				glBindTexture(GL_TEXTURE_2D, coin_texture);
+				glUniform1i(textureMapUniformLocation, 0);
+			
+				coin.Draw(
+					vertexPositionAttribute,
+					vertexNormalAttribute, textureCoordinateAttribute
+				);
+			}
+		}
+	}
+}
+
+
+void drawTank()
+{
+	//Apply Camera Manipluator to Set Model View Matrix on GPU
+	ModelViewMatrix.toIdentity();
+
+	//Apply Camera Manipluator to Set Model View Matrix on GPU
+	Matrix4x4 m = cameraManip.apply(ModelViewMatrix);
+
+	m.translate(tankPosition.x, 0.75, tankPosition.z);
+	m.rotate(tankRotation, 0, 1, 0); // Apply tank rotation around Y axis
+	m.scale(0.5, 0.5, 0.5);
+
+	glUniformMatrix4fv(
+		MVMatrixUniformLocation, //Uniform location
+		1, //Number of Uniforms
+		false, //Transpose Matrix
+		m.getPtr()); //Pointer to Matrix Values
+
+	// Bind box texture before drawing the box
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, hamvee_texture);
+	glUniform1i(textureMapUniformLocation, 0);
+
+	tank_chassis.Draw(
+		vertexPositionAttribute,
+		vertexNormalAttribute, textureCoordinateAttribute
+	);
+
+	// //Apply Camera Manipluator to Set Model View Matrix on GPU
+	// Matrix4x4 n = cameraManip.apply(ModelViewMatrix);
+
+	// n.translate(tankPosition.x, 0.5, tankPosition.z);
+	// n.rotate(wheelRotation, cos(tankRotation), 0, sin(tankRotation)); // Apply tank rotation around Y axis
+
+	// glUniformMatrix4fv(
+	// 	MVMatrixUniformLocation, //Uniform location
+	// 	1, //Number of Uniforms
+	// 	false, //Transpose Matrix
+	// 	n.getPtr()); //Pointer to Matrix Values
+
+
+	tank_front_wheel.Draw(
+		vertexPositionAttribute,
+		vertexNormalAttribute, textureCoordinateAttribute
+	);
+	tank_back_wheel.Draw(
+		vertexPositionAttribute,
+		vertexNormalAttribute, textureCoordinateAttribute
+	);
+
+	//Apply Camera Manipluator to Set Model View Matrix on GPU
+	Matrix4x4 o = cameraManip.apply(ModelViewMatrix);
+
+	o.translate(tankPosition.x, 0.75, tankPosition.z);
+	o.rotate(turretRotation, 0, 1, 0); // Apply tank rotation around Y axis
+	o.scale(0.5, 0.5, 0.5);
+
+	glUniformMatrix4fv(
+		MVMatrixUniformLocation, //Uniform location
+		1, //Number of Uniforms
+		false, //Transpose Matrix
+		o.getPtr()); //Pointer to Matrix Values
+
+	tank_turret.Draw(
+		vertexPositionAttribute,
+		vertexNormalAttribute, textureCoordinateAttribute
+	);
+}
 
 
 //! Keyboard Interaction
@@ -280,26 +422,6 @@ void keyboard(unsigned char key, int x, int y)
 	{
 		exit(0);
 	}
-	else if(key == 'a')
-	{
-		std::cout << "a key pressed" << std::endl;
-	}
-	else if(key == 'b')
-	{
-		glClearColor(1.0,1.0,1.0,1.0);
-	}
-	else if(key == 'B')
-	{
-		glClearColor(0.0,0.0,0.0,1.0);
-	}
-    else if(key == 'w')
-    {
-        glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
-    }
-    else if(key == 'W')
-    {
-        glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
-    }
 
 	glutPostRedisplay();
 
@@ -318,10 +440,56 @@ void keyUp(unsigned char key, int x, int y)
 void handleKeys()
 {
     //keys should be handled here
-	if(keyStates['a'])
+	if(keyStates['w'])
     {
-        
+        // Move tank forward in the direction it's facing
+		float radians = -tankRotation * (M_PI / 180.0f);
+		tankPosition.x -= sin(radians) * tankSpeed;
+		tankPosition.z += cos(radians) * tankSpeed;
+
+		wheelRotation += 5.0f; // Rotate wheels forward
+		
+		// Update camera focus to follow tank
+		updateCamera();
     }
+	if(keyStates['a'])
+	{
+		tankRotation += rotationSpeed; // Rotate left
+		// Update camera focus after rotation
+		turretRotation += rotationSpeed; // Rotate turret left
+		updateCamera();
+	}
+	if(keyStates['s'])
+	{
+		// Move tank backward in the direction it's facing
+		float radians = -tankRotation * (M_PI / 180.0f);
+		tankPosition.x += sin(radians) * tankSpeed;
+		tankPosition.z -= cos(radians) * tankSpeed;
+
+		wheelRotation -= 5.0f; // Rotate wheels backward
+		
+		// Update camera focus after moving backwards
+		updateCamera();
+	}
+	if(keyStates['d'])
+	{
+		tankRotation -= rotationSpeed; // Rotate right
+		// Update camera focus after rotation
+		turretRotation -= rotationSpeed; // Rotate turret right
+		updateCamera();
+	}
+	if(keyStates['j'])
+	{
+		turretRotation += rotationSpeed; // Rotate right
+		// Update camera focus after rotation
+		updateCamera();
+	}
+	if(keyStates['l'])
+	{
+		turretRotation -= rotationSpeed; // Rotate right
+		// Update camera focus after rotation
+		updateCamera();
+	}
 }
 
 //! Mouse Interaction
@@ -359,5 +527,15 @@ int printOglError(char *file, int line)
 		retCode = 1;
 	}
 return retCode;
+}
+
+// Update camera to follow tank
+void updateCamera() 
+{
+    // Calculate the position where the tank is
+    Vector3f tankWorldPos = Vector3f(tankPosition.x, 0.75, tankPosition.z); // Adjust Y value to match approximate tank height
+    cameraManip.setFocus(tankWorldPos);
+	cameraManip.setPanTiltRadius(turretRotation/(180/M_PI), -1.0f, 5.0f);
+	// cameraManip.setFocus(tank_chassis.getMeshCentroid());
 }
 
---
+++ common/Model.h
@@ -0,0 +1,26 @@
+// #ifndef MODEL_H_
+// #define MODEL_H_
+
+// #include "Mesh.h"
+// #include "Vector.h"
+// #include "Matrix.h"
+// #include <GL/glew.h>
+// #include <GL/glut.h>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+
+
+// class Model
+// {
+// private:
+//     mesh Mesh;
+//     Vector3f position;
+//     GLuint texture;
+
+// public:
+//     Model();
+//     ~Model();
+// }
+
+// #endif
